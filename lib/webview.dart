50be13e9070a9655743b44ddc1232729b9e56e63
chimaagu <agujeremiah7@gmail.com>
2024-11-18 15:26:15 +0100

    commit

---
+++ lib/webview.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _AppointmentWebViewState();
+}
+
+class _AppointmentWebViewState extends State<WebViewScreen> {
+  WebViewController? _controller;
+
+  @override
+  void initState() {
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onUrlChange: (change) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {
+            debugPrint(error.description.toString());
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse("https://billp.biz/"),
+      );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: _controller!,
+        ),
+      ),
+    );
+  }
+}
